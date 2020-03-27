0e524bc0cfe7771114e94305e9b7bd27f3652044
alwasdev <alwasdev@gmail.com>
2020-03-27 03:05:20 +0100

    moved fizzbuzz

--- src/test/java/com/techprimers/testing/FizzBuzzTest.java
+++ src/test/java/com/techprimers/testing/FizzBuzzTest.java
@@ -45,4 +45,18 @@ class FizzBuzzTest {
         fB = null;
     }
 
+
+    public class FizzBuzz {
+
+        public String play(int number) {
+
+            if (number == 0) throw new IllegalArgumentException("Number must not be 0");
+            if (number % 3 == 0) return "Fizz";
+            if (number % 5 == 0) return "Buzz";
+
+
+            return String.valueOf(number);
+        }
+    }
+
 }
